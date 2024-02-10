802d9aac7cad9e00b3fa502365eb499119b13936
sayako21 <63695015+sayako21@users.noreply.github.com>
2024-02-10 13:37:50 +0900

    一旦vueじゃなく、素のJSでやってみる (#13)
    Co-authored-by: sayako21 <>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,73 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue'
+
+export default defineComponent({
+  setup() {
+    // ページがロードされたときに実行される処理
+    onMounted(() => {
+      const queryOptions = { active: true, currentWindow: true }
+      chrome.tabs.query(queryOptions, (tabs) => {
+        console.log('inn')
+        // なんたらかんたら
+        const h1Elements = document.querySelectorAll('h1')
+
+        h1Elements.forEach((element) => {
+          console.log(h1Elements)
+
+          element.textContent = 'あ'
+        })
+      })
+
+      alert('test')
+      console.log(document)
+
+      //  // すべてのテキストを「あ」に変更する
+      //  const allTextElements = document.querySelectorAll('*')
+      //  allTextElements.forEach((element) => {
+      //    console.log(allTextElements)
+      //    element.textContent = 'あ'
+      //  })
+
+      //  // 例外的に h1 要素のテキストを元に戻す
+      //  const mainTitleElement = document.querySelector('h1')
+      //  if (mainTitleElement) {
+      //    mainTitleElement.textContent = 'Hello Vue 3 with Composition API and TypeScript!'
+      //  }
+    })
+
+    return {}
+  }
+})
+</script>
+
+<style>
+/* スタイルを適用する場合はここに記述 */
+</style>
+
+<!--<script setup lang="ts">
+import { ref } from 'vue'
+const color = ref('#000000')
+
+async function changeColor(color: string) {
+  // 現在のタブ情報を取得
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+
+  // 開いているタブのbodyの色を変更する
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id || 0 },
+    args: [color],
+    func: (args) => {
+      document.body.style.background = args
+    }
+  })
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="HomeView">
+    <h2>home</h2>
+    <div>
+      <input v-model="color" type="color" @update:model-value="changeColor($event)" />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+  </div>
+</template>-->
