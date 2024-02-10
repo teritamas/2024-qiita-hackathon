5ebe8e40f7daf7598cb5de9d7cdc0a8f42647521
sey323 <sey323_nm@yahoo.co.jp>
2024-02-10 16:15:35 +0900

    :+1: 幸せAPIで変換できる

--- test/content.js
+++ test/content.js
@@ -1,8 +1,51 @@
-window.addEventListener('load', function() {
-    const elements = document.querySelectorAll('.p-rich_text_section');
-    console.log(elements);
-    elements.forEach(element => {
-      console.log(element);
-      element.textContent = 'HAPPY';
+const settings = {
+  async: true,
+  crossDomain: true,
+  url: "http://127.0.0.1:8000/make_happy",
+  method: "POST",
+  headers: {
+    accept: "application/json",
+    "Content-type": "application/json",
+  },
+};
+
+// 初期設定
+const config = { childList: true, subtree: true };
+
+/**
+ * Slackのメッセージを取得し、ハッピーな文章に置き換える
+ */
+function processMakeHappy() {
+  // 変更があった場合に一致する全ての要素に対してリクエストを行う
+  const elements = document.querySelectorAll(".p-rich_text_section");
+  if (elements.length === 0) return;
+  console.log("変更対象:", elements.length, "件");
+
+  // それぞれの要素のテキストを取得
+  const messages = Array.from(elements).map((element) => element.textContent);
+
+  $.ajax({
+    ...settings,
+    data: JSON.stringify({ input_messages: messages }),
+  }).done(function (response) {
+    const happyMessages = response.results;
+
+    // 変更対象となるすべての要素に対して処理を行う
+    elements.forEach((element) => {
+      let currentValue = element.textContent;
+      // happyMessagesの各要素のうち、input_messageが同じものを取得
+      const happyMessage = happyMessages.find(
+        (happyMessage) => happyMessage.input_message === currentValue
+      );
+      console.log(happyMessage);
+      if (happyMessage) {
+        // その要素のテキストをhappyMessageのoutput_messageに変更
+        element.textContent = happyMessage.happy_message;
+      }
     });
+  });
+}
+
+addEventListener("load", function () {
+  processMakeHappy();
 });
