b42d7943df41271db7d5633ddb6e2693d53e66c7
Ichiro.S <sey323@users.noreply.github.com>
2024-02-10 13:41:37 +0900

    :+1: リクエスト形式を調整

--- backend/happy_world/main.py
+++ backend/happy_world/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from happy_world.models.make_happy_request import MakeHappyRequest
 from .models import make_happy_response
 from .facades.chatgpt import chatGPT
 
@@ -6,9 +7,9 @@ app = FastAPI()
 
 
 @app.post("/make_happy", response_model=make_happy_response.MakeHappyResponse)
-def post_make_happy(input_message: str):
-    happy_message: str = chatGPT.make_happy(input_message)
+def post_make_happy(make_request: MakeHappyRequest):
+    happy_message: str = chatGPT.make_happy(make_request.input_message)
     return {
         "happy_message": happy_message,
-        "input_message": input_message,
+        "input_message": make_request.input_message,
     }
