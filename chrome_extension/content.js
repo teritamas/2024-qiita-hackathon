386648dbcb2eabc40d72eaaad3383abc0a6d24f2
sayako21 <63695015+sayako21@users.noreply.github.com>
2024-02-10 21:17:09 +0900

    ローディング追加 (#26)
    Co-authored-by: sayako21 <>

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -41,14 +41,12 @@ function requestMakeHappy(elements) {
         element.appendChild(originalMessage);
       }
     });
+    setTimeout(hideLoader, 2000);
     return happyMessages;
   })
   .fail((jqXHR, textStatus, errorThrown) => {
     console.log('fail', jqXHR.status);
   })
-  .always(() => {
-    console.log('always');
-  });
 }
 
 /**
@@ -67,6 +65,8 @@ function processMakeHappy() {
 addEventListener("load", function () {
   // 2秒待ってから処理を開始
   setTimeout(processMakeHappy, 2000);
+  setTimeout(setLoading, 2000);
+  setTimeout(showLoader, 2000);
 });
 
 
@@ -78,5 +78,109 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.action === 'executeFunction') {
       // ここに実行したい関数を記述します
       processMakeHappy();
+      showLoader();
     }
 });
+
+// Function to show loader
+function showLoader() {
+    document.querySelector(".loader-box").style.display = "block";
+}
+
+// Function to hide loader
+function hideLoader() {
+    document.querySelector(".loader-box").style.display = "none";
+}
+
+function setLoading() {
+    var style = document.createElement("style");
+    style.innerHTML = `
+        /* CSS styles */
+        .loader-box {
+            display: none; /* Initially hidden */
+            position: fixed;
+            top: 0;
+            width: 100vw;
+            height: 100vh;
+            background: #bcceff63;
+            z-index: 100;
+        }
+
+        .loader {
+            width: 60px;
+            height: 60px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            z-index: 100;
+        }
+
+        .loader div {
+            width: 20px;
+            height: 20px;
+            float: left;
+            display: flex;
+            justify-content: center;
+            align-content:  center;
+            align-items: center;
+            position: relative;
+        }
+        
+        .load-span {
+            position: absolute;
+            display: block;
+            border-radius: 3px;
+            animation: anim 1.2s infinite;
+            animation-timing-function: linear;
+            z-index: 103;
+        }
+        
+        @keyframes anim {
+            0% { width:0px; height:0px; background: #18FFFF }
+            40% { width:15px; height:15px; background: #D500F9 }
+            80% { width:0px; height:0px; background: #18FFFF }
+        }
+        
+        .loader div:nth-child(2) .load-span,
+        .loader div:nth-child(4) .load-span {
+            animation-delay: 0.15s;
+        }
+        
+        .loader div:nth-child(3) .load-span,
+        .loader div:nth-child(5) .load-span,
+        .loader div:nth-child(7) .load-span {
+            animation-delay: 0.30s;
+        }
+        
+        .loader div:nth-child(6) .load-span,
+        .loader div:nth-child(8) .load-span {
+            animation-delay: 0.45s;
+        }
+        
+        .loader div:nth-child(9) .load-span {
+            animation-delay: 0.60s;
+        }
+    `;
+    
+    // Append style element to head
+    document.head.appendChild(style);
+    
+    // Create loader elements
+    var loaderBox = document.createElement("div");
+    loaderBox.classList.add("loader-box");
+    
+    var loader = document.createElement("div");
+    loader.classList.add("loader");
+    
+    for (var i = 0; i < 9; i++) {
+        var div = document.createElement("div");
+        var span = document.createElement("span");
+        span.classList.add("load-span");
+        div.appendChild(span);
+        loader.appendChild(div);
+    }
+
+    loaderBox.appendChild(loader);
+    document.body.appendChild(loaderBox);
+}
