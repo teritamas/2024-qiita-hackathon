29905a9474cc61603ce8cf100869556b98935b59
sey323 <sey323_nm@yahoo.co.jp>
2024-02-10 14:28:36 +0900

    :+1: Cors設定

--- backend/happy_world/main.py
+++ backend/happy_world/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from happy_world.models.make_happy_request import MakeHappyRequest
 from .models import make_happy_response
 from .facades.chatgpt import chatGPT
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/make_happy", response_model=make_happy_response.MakeHappyResponse)
