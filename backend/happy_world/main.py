ef6b6e09e8ccc48820cafc8883f28c8d8a4c5a65
Ichiro.S <sey323@users.noreply.github.com>
2024-02-10 22:30:43 +0900

    プロンプトでjson以外が返ってこない様に改善

--- backend/happy_world/main.py
+++ backend/happy_world/main.py
@@ -1,6 +1,7 @@
 from typing import List
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from happy_world.exceptions.convert_exception import ConvertException
 from happy_world.models.make_happy_request import MakeHappyRequest
 from .models.make_happy_response import MakeHappyMessageItem, MakeHappyResponse
 from .facades.chatgpt import chatGPT
@@ -17,8 +18,11 @@ app.add_middleware(
 
 @app.post("/make_happy", response_model=MakeHappyResponse)
 def post_make_happy(make_request: MakeHappyRequest):
-    happy_messages: List[MakeHappyMessageItem] = chatGPT.make_happy(
-        make_request.input_messages
-    )
-    print(happy_messages)
-    return MakeHappyResponse(results=happy_messages)
+    try:
+        happy_messages: List[MakeHappyMessageItem] = chatGPT.make_happy(
+            make_request.input_messages
+        )
+        return MakeHappyResponse(results=happy_messages)
+    except ConvertException as e:
+        print(e)
+        return MakeHappyResponse(results=[])
