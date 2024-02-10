326c7559e488cd541ccc5c55305516e3218b2690
sey323 <sey323_nm@yahoo.co.jp>
2024-02-11 00:52:05 +0900

    :sparkles: Cotoha API連携

--- backend/happy_world/main.py
+++ backend/happy_world/main.py
@@ -5,6 +5,7 @@ from happy_world.exceptions.convert_exception import ConvertException
 from happy_world.models.make_happy_request import MakeHappyRequest
 from .models.make_happy_response import MakeHappyMessageItem, MakeHappyResponse
 from .facades.chatgpt import chatGPT
+from .facades.cotoha import cotoha
 
 app = FastAPI()
 app.add_middleware(
@@ -19,8 +20,17 @@ app.add_middleware(
 @app.post("/make_happy", response_model=MakeHappyResponse)
 def post_make_happy(make_request: MakeHappyRequest):
     try:
+        # 感情分析
+        input_message_with_emotional_scores = [
+            cotoha.predict(input_message)
+            for input_message in make_request.input_messages
+        ]
+        
+        print(input_message_with_emotional_scores)
+        # 感情分析結果を元に、幸せなメッセージを生成
         happy_messages: List[MakeHappyMessageItem] = chatGPT.make_happy(
-            make_request.input_messages, make_request.positive_value_ratio
+            input_message_with_emotional_scores,
+            make_request.positive_value_ratio,
         )
         return MakeHappyResponse(results=happy_messages)
     except ConvertException as e:
